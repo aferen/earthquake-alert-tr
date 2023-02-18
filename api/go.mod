35a87e21157b21754ea52b0d118104950526fe5d
aferen <ahmetfurkanerenn@gmail.com>
2023-02-18 19:15:50 +0300

    initial commit

---
+++ api/go.mod
@@ -0,0 +1,10 @@
+module earthquake-alert
+
+go 1.18
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/umahmood/haversine v0.0.0-20151105152445-808ab04add26 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ api/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/PuerkitoBio/goquery"
+	"github.com/umahmood/haversine"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type Earthquake struct {
+	Date             time.Time
+	Coordinate       haversine.Coord
+	Depth            float64
+	MagnitudeMD      float64
+	MagnitudeML      float64
+	MagnitudeMW      float64
+	Region           string
+	DistancetoOrigin float64
+}
+
+var (
+	earthquakes        []Earthquake
+	nearestEarthquakes []Earthquake
+	earthquake         Earthquake
+	line               string
+	date               time.Time
+	coordinate         haversine.Coord
+	depth              float64
+	magnitudeMD        float64
+	magnitudeML        float64
+	magnitudeMW        float64
+	distanceToOrigin   float64
+	region             string
+	latitude           float64
+	longitude          float64
+	title              string
+	message            string
+)
+
+func main() {
+	listenAddr := os.Getenv("LISTEN_ADDR")
+	addr := listenAddr + `:` + os.Getenv("PORT")
+	http.HandleFunc("/send", sendNotification)
+	log.Printf("starting server at %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+func sendNotification(w http.ResponseWriter, r *http.Request) {
+	origin := haversine.Coord{Lat: 40.770727, Lon: 29.118538}
+	//origin := haversine.Coord{Lat: 37.444156, Lon: 37.188217}
+	maxDistance := 100.0
+	dateLayout := "2006.01.02T15:04:05 -0700"
+	timezone := "Europe/Istanbul"
+	maxTimeRange := 10.0 //minutes
+	location, err := time.LoadLocation(timezone)
+	now := time.Now().In(location)
+	sendMessage := false
+	counter := 0
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resp, err := http.Get("http://www.koeri.boun.edu.tr/scripts/lst0.asp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode == 200 || err != nil {
+		body, err := goquery.NewDocumentFromReader(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		data := strings.Trim(body.Find("pre").First().Text(), "\n")
+		scanner := bufio.NewScanner(strings.NewReader(data))
+		for scanner.Scan() {
+			if counter > 5 {
+				line = strings.TrimSpace(strings.Replace(scanner.Text(), " ", ",", -1))
+				for strings.Contains(line, ",,") {
+					line = strings.TrimSpace(strings.Replace(line, ",,", ",", -1))
+				}
+				arr := strings.Split(line, ",")
+				date, _ = time.Parse(dateLayout, strings.TrimSpace(arr[0])+"T"+strings.TrimSpace(arr[1])+" +0300")
+				latitude, _ = strconv.ParseFloat(strings.TrimSpace(arr[2]), 64)
+				longitude, _ = strconv.ParseFloat(strings.TrimSpace(arr[3]), 64)
+				coordinate = haversine.Coord{Lat: latitude, Lon: longitude}
+				depth, _ = strconv.ParseFloat(strings.TrimSpace(arr[4]), 64)
+				magnitudeMD, _ = strconv.ParseFloat(strings.TrimSpace(arr[5]), 64)
+				magnitudeML, _ = strconv.ParseFloat(strings.TrimSpace(arr[6]), 64)
+				magnitudeMW, _ = strconv.ParseFloat(strings.TrimSpace(arr[7]), 64)
+				region = strings.TrimSpace(arr[8]) + " " + strings.TrimSpace(arr[9])
+				_, distanceToOrigin := haversine.Distance(origin, coordinate)
+				earthquake = Earthquake{
+					date,
+					coordinate,
+					depth,
+					magnitudeMD,
+					magnitudeML,
+					magnitudeMW,
+					region,
+					distanceToOrigin,
+				}
+				if earthquake.DistancetoOrigin < maxDistance && earthquake.MagnitudeML > 1.0 {
+					if now.Sub(earthquake.Date).Minutes() > maxTimeRange {
+						sendMessage = true
+					}
+					nearestEarthquakes = append(nearestEarthquakes, earthquake)
+				}
+			}
+			counter++
+		}
+	} else {
+		log.Fatalf("failed to fetch data: %d %s", resp.StatusCode, resp.Status)
+	}
+	if sendMessage {
+		title = fmt.Sprintf("Deprem Oldu!!! Toplam: %d \n", len(nearestEarthquakes))
+
+		for _, earthquake = range nearestEarthquakes {
+			message = fmt.Sprintf("%s - %.1fML | %s | %s | Distance: %dkm\n", message, earthquake.MagnitudeML, earthquake.Date.Format("02/01/2006 15:04:05"), earthquake.Region, int(earthquake.DistancetoOrigin))
+		}
+	}
+
+	fmt.Println(message)
+}
