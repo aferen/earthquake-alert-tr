2e93750331e1b26e3e258b10c7bb310f03f0786e
aferen <ahmetfurkanerenn@gmail.com>
2023-02-19 00:44:09 +0300

    fixed cordova fcm plugin settings

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { FCM } from '@ionic-native/fcm/ngx';
+//import { FCM } from '@ionic-native/fcm/ngx';
+import { FCM } from "cordova-plugin-fcm-with-dependecy-updated/ionic";
+
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
+
 export class AppComponent {
   constructor(
       private platform: Platform,
       private splashScreen: SplashScreen,
       private statusBar: StatusBar,
-      private fcm: FCM
   ) {
     this.initializeApp();
   }
@@ -22,13 +25,14 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
       // subscribe to a topic
-      // this.fcm.subscribeToTopic('Deals');
+      FCM.subscribeToTopic('earthquake');
       // get FCM token
-      this.fcm.getToken().then(token => {
+      FCM.getToken().then(token => {
         console.log(token);
+        alert(token)
       });
       // ionic push notification example
-      this.fcm.onNotification().subscribe(data => {
+      FCM.onNotification().subscribe(data => {
         console.log(data);
         if (data.wasTapped) {
           console.log('Received in background');
@@ -37,11 +41,13 @@ export class AppComponent {
         }
       });
       // refresh the FCM token
-      this.fcm.onTokenRefresh().subscribe(token => {
+      FCM.onTokenRefresh().subscribe(token => {
         console.log(token);
+        alert(token)
       });
       // unsubscribe from a topic
       // this.fcm.unsubscribeFromTopic('offers');
     });
+
   }
 }
--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -12,6 +15,8 @@ import { FCM } from '@ionic-native/fcm/ngx';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [
     FCM,
+    SplashScreen,
+    StatusBar,
     {
       provide: RouteReuseStrategy,
       useClass: IonicRouteStrategy
